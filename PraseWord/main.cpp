314d464558468939e4f47d7874551cf37966e579
zhiwuba <xiabingliu@163.com>
2014-05-13 19:05:37 +0800

    nothing

--- PraseWord/main.cpp
+++ PraseWord/main.cpp
@@ -2,15 +2,17 @@
 #include <stdlib.h>
 #include <string.h>
 #include <string>
-
+#include <windows.h>
 #include "ParseText.h"
 
 int main()
 {
+	long start_time=GetTickCount();
 	ParseText parser;
-	parser.load_file_and_stat("D:\\Workspace\\App\\PraseWord\\samples\\fighting_utf8.txt");
+	parser.load_file_and_stat("D:\\Workspace\\App\\PraseWord\\samples\\ghost_utf8.txt");
 	parser.save_result_to_file("D:\\Workspace\\App\\PraseWord\\samples\\result.txt");
-
+	long cost_time=GetTickCount()-start_time;
+	printf("cost time is :%ld ms", cost_time);
 	return 0;
 }
 
